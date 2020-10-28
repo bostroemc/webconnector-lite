2ca6aae0d5a7d833828b75d92c8102338b6a91a8
bostroemc <cpbostrom@yahoo.com>
2020-10-28 10:17:40 -0500

    version 0.0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="webconnector-lite",
-    version="0.2",
+    version="0.4",
     packages=find_packages(),
     scripts=['bin/webconnector-lite']
 )
