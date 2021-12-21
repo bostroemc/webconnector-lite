5922c5ee53302f727ae858d95c4eb7d40894d1ee
bostroemc <carl.bostrom@boschrexroth-us.com>
2021-12-21 06:52:44 -0600

    update version to 0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="webconnector-lite",
-    version="0.4",
+    version="0.5",
     packages=find_packages(),
     scripts=['bin/webconnector-lite']
 )
