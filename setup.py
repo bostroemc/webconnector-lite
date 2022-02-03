253dbb550f1cb7c81dff7a5f41e92957ca9bc4fd
bostroemc <carl.bostrom@boschrexroth-us.com>
2022-02-03 05:52:59 -0600

    upgrade to core20

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="webconnector-lite",
-    version="0.5",
+    version="1.0",
     packages=find_packages(),
     scripts=['bin/webconnector-lite']
 )
