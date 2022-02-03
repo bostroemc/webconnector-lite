1cc37c38f864dc63cc8f53a183b7bc588309e6ff
bostroemc <carl.bostrom@boschrexroth-us.com>
2022-02-03 07:11:12 -0600

    change command to "bin/webconnector-lite" in snapcraft.yaml

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="webconnector-lite",
-    version="1.1",
+    version="1.0",
     packages=find_packages(),
     scripts=['bin/webconnector-lite']
 )
